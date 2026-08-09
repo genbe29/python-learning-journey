682223ca56811c64b0c9f03fd01873cc3fd63cb5
genbe29 <glebsemka7@gmail.com>
2026-08-09 18:37:35 +0500

    Calculator.py
    My first program on Python

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+num1 = int(input('Enter the first number: '))
+op = input('Enter the opperation ( =  -  /  *  ^ ): ')
+num2 = int(input('Enter the second number: '))
+
+if op == '+':
+    print(num1, op, num2, '=', (num1 + num2) )
+
+elif op == '-':
+    print(num1, op, num2, '=', (num1 - num2) )
+
+elif op == '*':
+    print(num1, op, num2, '=', (num1 * num2))
+
+elif op == '/':
+    if num2 != 0:
+        print(num1, op, num2, '=', (num1 / num2))
+    else:
+        print("You can't divide by zero!")
+
+elif op == '^':
+    print(num1, op, num2, '=', (num1 ** num2))
+
+else:
+    print('Please check your input data.')
