abc10ac2cc1abbce6afc1cd232478155ff4bccab
genbe29 <glebsemka7@gmail.com>
2026-08-12 21:04:30 +0500

    pygame_levels.py

---
+++ pygame_levels.py
@@ -0,0 +1,23 @@
+import pygame
+import time
+
+print(
+    'Choose one of the available levels:',
+    'Level 1',
+    'Level 2',
+    'Level 3',
+    sep='\n'
+)
+
+pygame.init()
+
+level = input()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Level', level)
+
+print('The program was executed successfully')
+
+time.sleep(5)
+
+pygame.quit()
