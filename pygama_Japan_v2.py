8dd71f6cab6f5746d897042734ec89d2de61ebad
genbe29 <glebsemka7@gmail.com>
2026-08-13 15:29:35 +0500

    pygama_Japan_v2.py

---
+++ pygama_Japan_v2.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame import FULLSCREEN
+a = int(input())
+b = int(input())
+pygame.init()
+
+screen = pygame.display.set_mode((a, b))
+pygame.display.set_caption('It is Japan')
+
+white = (255, 255, 255)
+red = (255, 0, 0)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(white)
+
+    pygame.draw.circle(screen, red, (a * 0.5, b * 0.5), a * 0.1)
+
+    pygame.display.flip()
+
+pygame.quit()
