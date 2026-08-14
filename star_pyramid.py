7351eeb5b08f859aa02cec73e7a7033c2e008da7
genbe29 <glebsemka7@gmail.com>
2026-08-14 19:54:47 +0500

    star_pyramid.py

--- star_pyramid.py
+++ star_pyramid.py
@@ -3,10 +3,12 @@ num = int(input("Enter your odd number: "))
 half_num = int((num / 2) - 0.5)
 
 for i in range(num):
+    
     if i + 1 < num / 2 + 1:
         for j in range(i + 1):
             print('*', end='')
         print()
+        
     else:
         for k in range(half_num, 0, -1):
             print('*' * k)
